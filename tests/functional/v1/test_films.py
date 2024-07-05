0d7e5dbc0197acfc5d413e7fefc9e2f69c621dc1
ArtemIsmagilov <artem.ismagilov.2000@mail.ru>
2024-07-05 05:02:22 +0400

    testing `list films`

--- tests/functional/v1/test_films.py
+++ tests/functional/v1/test_films.py
@@ -1,5 +1,8 @@
 def test_films(client, api_v1_films):
-    assert client.get(api_v1_films + "/").status_code == 200
+    # list all films from 0 with size 10
+    response = client.get(api_v1_films + "/", params={"page_number": 0, "page_size": 10})
+    assert response.status_code == 200
+    assert len(response.json()) == 10
 
     # list films with filters, paginate and sorting
     response = (
@@ -7,7 +10,7 @@ def test_films(client, api_v1_films):
             api_v1_films + "/",
             params={
                 "sort": "id",
-                "page_number": 1,
+                "page_number": 0,
                 "page_size": 10,
                 "genre": "Biography",
                 "actor": "Barbara Church",
@@ -16,5 +19,8 @@ def test_films(client, api_v1_films):
             },
         )
     )
+    assert response.status_code == 200
 
-    print(response)
+    # fetch data from redis
+    response = client.get(api_v1_films + "/")
+    assert response.status_code == 200
