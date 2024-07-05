433e2eab726b935b65b1f085a15fa776cf26e68e
ArtemIsmagilov <artem.ismagilov.2000@mail.ru>
2024-07-05 05:24:27 +0400

    testing films by person

--- tests/functional/v1/test_films.py
+++ tests/functional/v1/test_films.py
@@ -1,26 +1,120 @@
-def test_films(client, api_v1_films):
+def test_list_films(client, api_v1_films, endpoint="/"):
     # list all films from 0 with size 10
-    response = client.get(api_v1_films + "/", params={"page_number": 0, "page_size": 10})
+    response = client.get(
+        api_v1_films + endpoint, params={"page_number": 0, "page_size": 10}
+    )
     assert response.status_code == 200
     assert len(response.json()) == 10
 
     # list films with filters, paginate and sorting
-    response = (
-        client.get(
-            api_v1_films + "/",
-            params={
-                "sort": "id",
-                "page_number": 0,
-                "page_size": 10,
-                "genre": "Biography",
-                "actor": "Barbara Church",
-                "director": "Angela Turner",
-                "writer": "Angela Turner",
-            },
-        )
+    response = client.get(
+        api_v1_films + endpoint,
+        params={
+            "sort": "id",
+            "page_number": 0,
+            "page_size": 10,
+            "genre": "Biography",
+            "actor": "Barbara Church",
+            "director": "Angela Turner",
+            "writer": "Angela Turner",
+        },
     )
     assert response.status_code == 200
 
     # fetch data from redis
-    response = client.get(api_v1_films + "/")
+    response = client.get(api_v1_films + endpoint)
     assert response.status_code == 200
+
+    # not exists data
+    response = client.get(
+        api_v1_films + endpoint,
+        params={
+            "sort": "id",
+            "page_number": 0,
+            "page_size": 10,
+            "genre": "fake",
+            "actor": "fake",
+            "director": "fake",
+            "writer": "fake",
+        },
+    )
+    assert response.status_code == 404
+
+
+def test_search_films(client, api_v1_films, endpoint="/search"):
+    # search all films from 0 with size 10
+    response = client.get(
+        api_v1_films + endpoint, params={"page_number": 0, "page_size": 10}
+    )
+    assert response.status_code == 200
+    assert len(response.json()) == 10
+
+    # search films with filters, paginate and sorting
+    response = client.get(
+        api_v1_films + endpoint,
+        params={
+            "query": "Star",
+            "sort": "id",
+            "page_number": 0,
+            "page_size": 10,
+            "genre": "Biography",
+            "actor": "Barbara Church",
+            "director": "Angela Turner",
+            "writer": "Angela Turner",
+        },
+    )
+    assert response.status_code == 200
+
+    # fetch data from redis
+    response = client.get(api_v1_films + endpoint)
+    assert response.status_code == 200
+
+    # not exists data
+    response = client.get(
+        api_v1_films + endpoint,
+        params={
+            "sort": "id",
+            "page_number": 0,
+            "page_size": 10,
+            "genre": "fake",
+            "actor": "fake",
+            "director": "fake",
+            "writer": "fake",
+        },
+    )
+    assert response.status_code == 404
+
+
+def test_person_films(client, api_v1_films, endpoint='/37c36461-9a0d-4fd9-b257-fae0b2b6e8ad/film'):
+    # by person films from 0 with size 10
+    response = client.get(
+        api_v1_films + endpoint, params={"page_number": 0, "page_size": 10}
+    )
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+
+    # by person films with filters, paginate and sorting
+    response = client.get(
+        api_v1_films + endpoint,
+        params={
+            "sort": "id",
+            "page_number": 0,
+            "page_size": 10,
+        },
+    )
+    assert response.status_code == 200
+
+    # fetch data from redis
+    response = client.get(api_v1_films + endpoint)
+    assert response.status_code == 200
+
+    # not exists data
+    response = client.get(
+        api_v1_films + '37c36461-9a0d-4fd9-b257-fae0b2b6e999',
+        params={
+            "sort": "id",
+            "page_number": 0,
+            "page_size": 10,
+        },
+    )
+    assert response.status_code == 404
