dad177f174149733ba8928a98963e4365f21f3e3
ArtemIsmagilov <artem.ismagilov.2000@mail.ru>
2024-07-05 15:11:02 +0400

    page number=0 and size=10 already sets

--- tests/functional/v1/test_films.py
+++ tests/functional/v1/test_films.py
@@ -1,6 +1,6 @@
 def test_list_films(client, api_v1_films, endpoint='/'):
     # list all films from 0 with size 10
-    response = client.get(api_v1_films + endpoint, params={'page_number': 0, 'page_size': 10})
+    response = client.get(api_v1_films + endpoint)
     assert response.status_code == 200
     assert len(response.json()) == 10
 
@@ -41,7 +41,7 @@ def test_list_films(client, api_v1_films, endpoint='/'):
 
 def test_search_films(client, api_v1_films, endpoint='/search'):
     # search all films from 0 with size 10
-    response = client.get(api_v1_films + endpoint, params={'page_number': 0, 'page_size': 10})
+    response = client.get(api_v1_films + endpoint)
     assert response.status_code == 200
     assert len(response.json()) == 10
 
@@ -83,7 +83,7 @@ def test_search_films(client, api_v1_films, endpoint='/search'):
 
 def test_person_films(client, api_v1_films, endpoint='/37c36461-9a0d-4fd9-b257-fae0b2b6e8ad/film'):
     # by person films from 0 with size 10
-    response = client.get(api_v1_films + endpoint, params={'page_number': 0, 'page_size': 10})
+    response = client.get(api_v1_films + endpoint)
     assert response.status_code == 200
     assert len(response.json()) == 1
 
--- tests/functional/v1/test_genres.py
+++ tests/functional/v1/test_genres.py
@@ -1,6 +1,6 @@
 def test_list_genres(client, api_v1_genres, endpoint='/'):
     # list all genres from 0 with size 10
-    response = client.get(api_v1_genres + endpoint, params={'page_number': 0, 'page_size': 10})
+    response = client.get(api_v1_genres + endpoint)
     assert response.status_code == 200
     assert len(response.json()) == 10
 
@@ -8,6 +8,40 @@ def test_list_genres(client, api_v1_genres, endpoint='/'):
     response = client.get(
         api_v1_genres + endpoint,
         params={
+            'sort': 'id',
+            'page_number': 0,
+            'page_size': 10,
+        },
+    )
+    assert response.status_code == 200
+
+    # fetch data from redis
+    response = client.get(api_v1_genres + endpoint)
+    assert response.status_code == 200
+
+    # not exists data, to large page_number
+    response = client.get(
+        api_v1_genres + endpoint,
+        params={
+            'sort': 'id',
+            'page_number': 99,
+            'page_size': 10,
+        },
+    )
+    assert response.status_code == 404
+
+
+def test_search_genres(client, api_v1_genres, endpoint='/search'):
+    # search all genres from 0 with size 10
+    response = client.get(api_v1_genres + endpoint)
+    assert response.status_code == 200
+    assert len(response.json()) == 10
+
+    # search genres with filters, paginate and sorting by title
+    response = client.get(
+        api_v1_genres + endpoint,
+        params={
+            'query': 'Action',
             'sort': 'id',
             'page_number': 0,
             'page_size': 10,
