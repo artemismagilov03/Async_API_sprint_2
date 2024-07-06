47788237e758d2a94a079468551727ed603d900f
ArtemIsmagilov <118372045+ArtemIsmagilov@users.noreply.github.com>
2024-07-06 10:55:28 +0400

    pytest fixtures will be created once

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,27 +7,27 @@ sys.path.append('movie_service/movies_api')
 from movies_api.main import app
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def client():
     with TestClient(app) as client_app:
         yield client_app
 
 
 @pytest.fixture
-def fake_uuid():
+def fake_uuid(scope='session'):
     return '0031feab-8f53-412a-8f53-47098a60a999'
 
 
 @pytest.fixture
-def api_v1_films():
+def api_v1_films(scope='session'):
     return '/api/v1/films'
 
 
 @pytest.fixture
-def api_v1_genres():
+def api_v1_genres(scope='session'):
     return '/api/v1/genres'
 
 
 @pytest.fixture
-def api_v1_persons():
+def api_v1_persons(scope='session'):
     return '/api/v1/persons'
